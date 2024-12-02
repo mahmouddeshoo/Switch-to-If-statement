e02ba5b7ff8ffa0ed67e93f1d2da735b34a74f69
mahmouddeshoo <mahmouddshoo@gmail.com>
2024-12-02 20:35:56 +0300

    This for task Switch to if statement

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// الوصف
+// let fruit = “apple”;
+
+// switch (fruit) {
+// case “apple”:
+// case “pear”:
+// console.log(“This is a pome fruit.”);
+// break;
+// case “banana”:
+// case “mango”:
+// console.log(“This is a tropical fruit.”);
+// break;
+// default:
+// console.log(“Unknown fruit.”);
+// }
+
+// قم بتحويل هذا الكود الى IF ELSE
+
+
+
+ let fruit = "apple";
+if(fruit === "apple" || fruit === "pear"){
+    console.log("This is a pome fruit.");
+}
+else if(fruit === "banana" || fruit === "mango"){
+    console.log("This is a tropical fruit.");
+    }
+    else{
+        console.log("Unknown fruit.");
+        }
